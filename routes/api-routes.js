faaaa7181afc2b1b2ebb3ceb6ee6e15dadab38a7
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-25 09:57:22 +0100

    Creation of api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const db = require("../models");
 
-module.exports = app => {
-    app.get('/api/workouts', (req, res) => {
-        
+module.exports = (app) => {
+  app.get("/api/workouts", async (req, res) => {
+    try {
+      const results = await db.Workout.find({});      
+      res.json(results);
+    } catch (err) {
+      console.error("ERROR - api-routes.js - /api/workouts get: ", err);
+      res.status(404).end();
+    }
+  });
+
+  app.put("/api/workouts/:id", async (req, res) => {
+    try {
+      const result = await db.Workout.findByIdAndUpdate(req.params.id, {
+        $push: { exercises: req.body },
       });
-    
-    app.get('/api/workouts/:id', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
+      res.json(result);
+    } catch (err) {
+      console.error("ERROR - api-routes.js - /api/workouts/:id put: ", err);
+      res.status(404).end();
+    }
+  });
 
-    app.get('/api/workouts/range', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
+  app.post("/api/workouts", async (req, res) => {
+    try {
+      console.log(req.body);
+      const result = await db.Workout.create(req.body);
+      res.json(result);
+    } catch (err) {
+      console.error("ERROR - api-routes.js - /api/workouts post: ", err);
+      res.status(404).end();
+    }
+  });
 
-    app.post('/api/workouts', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-    
-    app.put('/api/workouts/:id', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-}
+  app.get("/api/workouts/range", async (req, res) => {
+    try {
+      console.log("get request pending");
+      const results = await db.Workout.find({});
+      console.log(results);
+      res.json(results);
+    } catch (err) {
+      console.error("ERROR - api-routes.js - /api/workouts/range get: ", err);
+      res.status(404).end();
+    }
+  });  
+};
